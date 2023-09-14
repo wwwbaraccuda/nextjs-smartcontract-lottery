bbae95c5d69a81e77187da945f3d0b2a74269cc6
wwwbaraccuda <vincentiusardian@gmail.com>
2023-09-14 16:25:46 +0700

    first commit

---
+++ components/LotteryEntrance.js
@@ -0,0 +1,13 @@
+import { useWeb3Contract } from "react-moralis"
+
+export default function LotteryEntrance() {
+    // const {runContractFunction: enterRaffle} = useWeb3Contract({
+    //     abi:
+    //     contractAddress:
+    //     functionName:
+    //     params: {}
+    //     msgValue:
+    // })
+
+    return <div>Hi from Lottery Entrance</div>
+}
